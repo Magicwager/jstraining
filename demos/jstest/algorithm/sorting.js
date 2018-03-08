1606ce7c071aba622bd497cbb1c105515e7846ee
wujiank <wujiank@yonyou.com>
2018-03-08 11:12:46 +0800

    提交快速排序算法

--- demos/jstest/algorithm/sorting.js
+++ demos/jstest/algorithm/sorting.js
@@ -1,4 +1,8 @@
 /* 排序算法 */
+
+/* 冒泡排序 
+从某一边挨个确定正确的位置，挨个询问是不是正确的顺序，直到全部问完，然后接着下一个基数挨个询问
+*/
 function bubbleSort(array){
     for(var i=0;i<array.length-1;i++){
         for(var j=i+1;j<array.length;j++){
@@ -8,22 +12,32 @@ function bubbleSort(array){
                 array[j]=temp;
                 
             }
-            console.log(array)
+         /*    console.log(array) */
         }
     }
     return array
 }
-/* function bubbleSort(arr) {  
-    for(let i = 0,l=arr.length;i<l-1;i++) {
-        for(let j = i+1;j<l;j++) { 
-          if(arr[i]>arr[j]) {
-                let tem = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tem;
+
+/* 快速排序
+参考某个元素值，将小于它的值，放到左数组中，大于它的值的元素就放到右数组中，然后递归进行上一次左右数组的操作，返回合并的数组就是已经排好顺序的数组了
+*/
+function quickSort(arr) {
+        if(arr.length<=1) {
+            return arr;
+        }
+        let leftArr = [];
+        let rightArr = [];
+        let q = arr[0];
+        for(let i = 1,l=arr.length; i<l; i++) {
+            if(arr[i]>q) {
+                rightArr.push(arr[i]);
+            }else{
+                leftArr.push(arr[i]);
             }
         }
+        return [].concat(quickSort(leftArr),[q],quickSort(rightArr));
     }
-    return arr;
-} */
 var a=["3","4","2",'5','1']
 console.log(bubbleSort(a))
+var b=[3,4,2,5,1,34,23,45,232]
+console.log(quickSort(b))
